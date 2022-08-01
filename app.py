a1b0c68a3f5213842abdd00b2016e43c861bb64e
david <davidgrong778@gmail.com>
2022-07-31 21:40:41 -0500

    finished part 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, redirect, render_template, request
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -20,6 +20,10 @@ def home():
     users = User.query.all()
     return render_template('users.html', users=users)
 
+@app.route('/users/new', methods=["GET"])
+def new_user():
+    return render_template('new-user.html')
+
 @app.route('/users/new', methods=["POST"])
 def create_user():
     first_name = request.form['first-name']
@@ -32,11 +36,6 @@ def create_user():
     db.session.add(new_user)
     db.session.commit()
     return redirect(f"/users/{new_user.id}")
-
-@app.route('/users/new', methods=["GET"])
-def new_user():
-    return render_template('new-user.html')
-
 
 @app.route('/users/<int:user_id>')
 def user_details(user_id):
@@ -69,4 +68,54 @@ def delete_user(user_id):
     db.session.commit()
     
     return redirect('/users')
+
+@app.route('/users/<int:user_id>/posts/new')
+def post_form(user_id):
+    user = User.query.get_or_404(user_id)
+
+    return render_template('post-form.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def add_new_post(user_id):
+    title = request.form['title']
+    content = request.form['content']
+
+    new_post = Post(title=title, content=content, user_id=user_id)
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(f'/posts/{new_post.id}')
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post-details.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('edit-post.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    title = request.form['title']
+    content = request.form['content']
+    if(title):
+        post.title = title
+    if(content):
+        post.content = content
+    
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f'/posts/{post_id}')
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f'/users/{post.user_id}')
 
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 """Models for Blogly."""
+import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
@@ -15,3 +17,13 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=True, 
                             default='https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png')
+    posts = db.relationship('Post', backref='user')
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
